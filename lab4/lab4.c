ad10e58fc94a8613ce7a6b6e7fd5d7c88c1c17e0
up202206230 <up202206230@fe.up.pt>
2024-04-18 17:26:06 +0100

    mouse_test_async() done (not VF)

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -10,6 +10,7 @@
 
 extern struct packet final_packet; // Defined in KBC_mouse.c
 uint8_t status = 0;
+extern int counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -95,9 +96,75 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
+  int ipc_status, r;
+  message msg;
+  uint8_t seconds = 0;
+  uint8_t mouse_irq_set = 0, timer_irq_set = 0; 
+
+  // if (mouse_write_command(ENABLE_DR) != 0) {
+  //   return 1;
+  // }
+
+  if (mouse_enable_data_reporting() != 0) {
     return 1;
+  }
+
+  if (mouse_subscribe(&mouse_irq_set) != 0) {
+    return 1;
+  }
+
+  if (timer_subscribe_int(&timer_irq_set) != 0) {
+    return 1;
+  }
+
+  while (seconds < idle_time) {
+    if (driver_receive(ANY, &msg, &ipc_status) != 0) {
+      continue;
+    }
+
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & timer_irq_set) {
+            timer_int_handler();
+            if (counter % sys_hz() == 0) {
+              seconds++;
+            }
+          }
+          if (msg.m_notify.interrupts & mouse_irq_set) {
+            if (util_sys_inb(KBC_STATUS_REG, &status) != 0) { // We test the function that reads the status from the status register, to check if we didn't have a communication error
+              printf("Could not read status");
+              return 1;
+            }
+            mouse_ih();
+            r = mouse_build_packet();
+            if (r == 0) {
+              break;
+            }
+            else if (r == 1) {
+              return 1;
+            }
+            else if (r == 2) {
+              mouse_print_packet(&final_packet);
+            }
+            seconds = 0;
+            counter = 0;
+          }
+      }
+    }
+  }
+  if (mouse_write_command(DISABLE_DR) != 0) {
+    return 1;
+  }
+
+  if (timer_unsubscribe_int() != 0) {
+    return 1;
+  }
+
+  if (mouse_unsubscribe() != 0) {
+    return 1;
+  }
+  return 0;
 }
 
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
